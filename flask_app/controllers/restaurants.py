634746591cbbba41ca837f2b6dd4da6971071cc1
NickBarkerDev <nick.barker.devops@gmail.com>
2022-07-18 05:46:29 -0700

    tentatively created all neccessary visible and hidden routes

--- flask_app/controllers/restaurants.py
+++ flask_app/controllers/restaurants.py
@@ -1,3 +1,44 @@
 from flask_app import app
-from flask import render_template, request, redirect_users
+from flask import render_template, request, redirect
 from flask_app.models import restaurant
+
+# VISIBLE ROUTES
+# Redirect from root path
+@app.route('/')
+def home():
+    return redirect('/dojos')
+
+
+# Dojos Page
+@app.route('/dojos')
+def dojos():
+    return render_template('/new_dojo_all_dojos.html')
+
+
+# Create Ninja page
+@app.route('/create_ninja')
+def create_ninja():
+    return render_template('/new_ninja.html')
+
+
+# View one Dojo and all it's Ninjas
+@app.route('/show_dojo')
+def show_dojo():
+    return render_template('one_dojo.html')
+
+
+
+
+
+# HIDDEN ROUTES
+
+# Create Dojo
+@app.route('/process_create_dojo')
+def process_create_dojo():
+    return redirect('/dojos')
+
+
+# Create Ninja
+@app.route('/process_create_ninja')
+def create_ninja():
+    return redirect('show_dojo')
