eb18f97334e375318f3e565ced8e751899a263fd
NickBarkerDev <nick.barker.devops@gmail.com>
2022-07-18 05:11:15 -0700

    initial commit

---
+++ flask_app/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask
+app = Flask(__name__)
+app.secret_key = "gVkYp3s6v9y$B&E)H@McQeThWmZq4t7w" #256-bit key
---
+++ flask_app/controllers/restaurants.py
@@ -0,0 +1,3 @@
+from flask_app import app
+from flask import render_template, request, redirect_users
+from flask_app.models import restaurant
